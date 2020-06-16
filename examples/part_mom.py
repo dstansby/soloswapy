8c1c4b84fefcc6f149d02bc304fc655a8abec98d
David Stansby <dstansby@gmail.com>
2020-06-16 10:23:06 +0100

    Create examples folder

---
+++ examples/part_mom.py
@@ -0,0 +1,4 @@
+from soloswapy.data import load
+
+moms = load('/Users/dstansby/Data/solo/swa/eas/solo_L1_swa-eas-OnbPartMoms_20200606031700-20200606224740_V01.cdf')
+moms.peek()
--- test.py
+++
@@ -1,5 +0,0 @@
-from soloswapy.dist import DistributionFunction
-import matplotlib.pyplot as plt
-
-dist = DistributionFunction('/Users/dstansby/Data/solo/swa/eas/solo_L1_swa-eas1-NM3D_20200506121000-20200506191440_V01.cdf')
-dist.peek(energies=[32, 60])
